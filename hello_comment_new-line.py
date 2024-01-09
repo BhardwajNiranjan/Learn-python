288e0ac71a3423bf8b35526e69662c8cd512bbe3
Niranjan Bhardwaj <116885634+BhardwajNiranjan@users.noreply.github.com>
2024-01-09 12:35:20 +0530

    first hello program in python

---
+++ hello_comment_new-line.py
@@ -0,0 +1,35 @@
+#simple hello statement print
+print("hello dost- This is our first program")
+ 
+
+#    Single-Line Comments
+#   commnent # sign shows line i9s commented, So # is used to comment any line of code
+#    print("hello dost- This is our first program") this is called single line comment 
+
+#    multiline comment 
+
+"""
+this is our first program in multiline comment
+can u tell me about this
+is it right or not 
+give one line answer
+"""
+
+
+
+
+
+# print a line in to next new line 
+print("ram ram mitro : \nwe are printing a line into new line")
+
+
+print("Name of Student who is present in class: \nNiranjan \nVikrant \nVijay \nShubham \nPuru")
+
+
+
+
+
+
+
+
+
