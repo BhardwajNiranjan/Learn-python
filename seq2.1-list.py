df1455c5d440fe3f5ed4820e6878455557918a76
Niranjan Bhardwaj <116885634+BhardwajNiranjan@users.noreply.github.com>
2024-01-09 12:51:24 +0530

    To Learn about List Sequence Data Types
    To Learn about List Sequence Data Types and its working

---
+++ seq2.1-list.py
@@ -0,0 +1,15 @@
+print("----------------- Sequence -------------------------")
+print("-------------- Type of Sequence - String, List and Touple --------------")
+
+print("*****************************************************************************")
+
+print("----------> Secomd is List <----------")
+
+print(" list is an ordered sequence of items a = [a,b,c]")
+
+list = [ "niranjan", "vijay", "vikrant", "puru" ]
+
+print(list , type(list))
+print(list)
+list1 = [ "niranjan", "vijay", "vikrant", "puru", "123", "23.34", "hi34",]
+print(list1)
