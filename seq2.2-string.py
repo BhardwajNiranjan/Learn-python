5efb24cf5720d19c1bbdff9e3b7f5ac316f160ae
Niranjan Bhardwaj <116885634+BhardwajNiranjan@users.noreply.github.com>
2024-01-09 12:52:09 +0530

    To Learn about String Sequence Data Types
    To Learn about String Sequence Data Types and working

---
+++ seq2.2-string.py
@@ -0,0 +1,15 @@
+print("----------------- Sequence -------------------------")
+
+print("-------------- Type of Sequence - String, List and Touple --------------")
+
+print("----------> First is String - collection one or more character put in a single quotes, double quotes & triple quotes <----------")
+
+S1='this is single quotes' # sinlge qoutes means - string
+
+S2="this is double quotes"
+S3='''
+this is triple quote
+'''
+print(S1)
+print(S2)
+print(S3)
