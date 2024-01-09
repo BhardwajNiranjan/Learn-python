00fff9d8dd4f0e526347a179287c9a91b397b3c0
Niranjan Bhardwaj <116885634+BhardwajNiranjan@users.noreply.github.com>
2024-01-09 12:45:44 +0530

    Topic -  Floating Number in Data Types

---
+++ num1.2-float.py
@@ -0,0 +1,10 @@
+
+print("----------------- numbers -------------------------")
+print("--------------Type of Numbers - Integer, Complex Number and Floats Number --------------")
+print("*****************************************************************************")
+print("----------> Second is Floats Number <----------")
+
+A=10.7
+C = 14.09
+print(A ,type(A))
+print(C ,type(C))
