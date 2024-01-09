fe7bb63a8dc69e5f6790edb86643edd4f49d050f
Niranjan Bhardwaj <116885634+BhardwajNiranjan@users.noreply.github.com>
2024-01-09 12:55:16 +0530

    Topic - Set Data Types and function

---
+++ set_data-type4.py
@@ -0,0 +1,17 @@
+print("The data type's unordered collection is Python Set. It is iterable, mutable(can change after creation), and has remarkable components.")
+
+print("----------------------------------------------------------------------------------------------------")
+
+print("The elements of a set have no set order; It might return the element's altered sequence.")
+
+print("----------------------------------------------------------------------------------------------------")
+
+print("Either a sequence of elements is passed through the curly braces and separated by a comma to create the set or the built-in function set() is used to create the set.")
+
+print("----------------------------------------------------------------------------------------------------")
+
+print(" It can contain different kinds of values.")
+
+
+set1 = set()  
+print(set1)
