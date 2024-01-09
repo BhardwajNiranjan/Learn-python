9b69d69f3fe705142831068071f1bba56a57e632
Niranjan Bhardwaj <116885634+BhardwajNiranjan@users.noreply.github.com>
2024-01-09 12:47:17 +0530

    Topic -  Complex Number in Data Types

---
+++ num1.3-complex.py
@@ -0,0 +1,9 @@
+print("----------------- numbers -------------------------")
+print("--------------Type of Numbers - Integer, Complex Number and Floats Number --------------")
+print("*****************************************************************************")
+print("----------> Third is complex Number <----------")
+
+G=2+5j
+E=6+4j
+print(G ,type(G))
+print(E ,type(E))
