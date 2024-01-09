25b7e3747e6facfe96a55084b99c5341c4dbddea
Niranjan Bhardwaj <116885634+BhardwajNiranjan@users.noreply.github.com>
2024-01-09 12:49:52 +0530

    To Learn about Sequence Data Types and Types
    To Learn about Sequence Data Types and Types - List, String and Touple...

---
+++ data_type-sequence2.py
@@ -0,0 +1,19 @@
+print("here we  will discuss about Sequence in data type")
+print("*****************************************************************************")
+
+
+
+print("-----------------> Sequence <---------------------")
+print("-------------- Type of Sequence --> String, List and Touple Sequence --------------")
+
+print("----------------------------------------------------------------------------------------------------------------------------------------")
+
+print(" String - The sequence of characters in the quotation marks can be used to describe the string. A string can be defined in Python using single, double, or triple quotes.")
+
+print("----------------------------------------------------------------------------------------------------------------------------------------")
+
+print(" Lists in Python are like arrays in C, but lists can contain data of different types. The things put away in the rundown are isolated with a comma (,) and encased inside square sections [] ")
+
+print("----------------------------------------------------------------------------------------------------------------------------------------")
+
+print(" Tuple - a tuple is like a list. Tuples, like lists, also contain a collection of items from various data types. A parenthetical space () separates the tuple's components from one another.")
