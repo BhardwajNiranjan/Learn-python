db332f5d1105fcb0feb05f3384acd3eaed6cf9f7
Niranjan Bhardwaj <116885634+BhardwajNiranjan@users.noreply.github.com>
2024-01-09 12:44:59 +0530

    Topic - Integer Number in Data Types

---
+++ num1.1-int.py
@@ -0,0 +1,8 @@
+print("----------------- numbers -------------------------")
+print("--------------Type of Numbers - Integer, Complex Number and Floats Number --------------")
+print("----------> First is Integer Number <----------")
+
+A=10
+B = 14
+print(A ,type(A))
+print(B ,type(B))
