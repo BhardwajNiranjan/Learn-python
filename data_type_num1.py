5df3493a7cb8d0756f7f6f82dc6cfc119deb8c0f
Niranjan Bhardwaj <116885634+BhardwajNiranjan@users.noreply.github.com>
2024-01-09 12:43:21 +0530

    Learn Number Data Type And its Types
    To Learn Number Data Type And its Types  - Integer Number, Floating Number, and Complex Number

---
+++ data_type_num1.py
@@ -0,0 +1,24 @@
+print("here we  will discuss about numbers in data type")
+print("*****************************************************************************")
+
+
+print("----------------- numbers -------------------------")
+print("--------------Type of Numbers - Integer, Complex Number and Floats Number --------------")
+
+print("----------------------------------------------------------------------------------------------------------------------------------------")
+
+print("Int: Whole number worth can be any length, like numbers 10, 2, 29, - 20, - 150, and so on. An integer can be any length you want in Python. Its worth has a place with int.")
+
+print("----------------------------------------------------------------------------------------------------------------------------------------")
+
+print("Float: Float stores drifting point numbers like 1.9, 9.902, 15.2, etc. It can be accurate to within 15 decimal places.")
+
+print("----------------------------------------------------------------------------------------------------------------------------------------")
+
+print("Complex: An intricate number contains an arranged pair, i.e., x + iy, where x and y signify the genuine and non-existent parts separately. The complex numbers like 2.14j, 2.0 + 2.3j, etc.")
+
+
+
+
+
+
